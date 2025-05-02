28b5ae3bc640a9d6e027e0a8ba967a15084ad8a2
Chiu Yu-Chien <107299163+YuChien-0327@users.noreply.github.com>
2025-05-02 11:47:23 +0800

    Update string_compression.cpp

--- leetcode75/string_compression.cpp
+++ leetcode75/string_compression.cpp
@@ -30,5 +30,20 @@ public:
         return idx;
 
         /* 解法二：最佳解 */
+        int idx=0, start_idx=0, len=chars.size();
+        for(int i=0;i<len; i++){
+            // 如果字元和下一字元不一樣 or 最後一個字
+            if(i+1==len || chars[i] != chars[i+1]){
+                chars[idx++] = chars[i];
+                int count = i - start_idx + 1;
+                if (count > 1) {
+                    for (char c : to_string(count)) {
+                        chars[idx++] = c;
+                    }
+                }
+                start_idx = i + 1;
+            }
+        }
+        return idx;
     }
 };
